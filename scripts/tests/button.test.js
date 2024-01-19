7b882a5b4dd2d31e23f0eea2e94cdf7ea6d4085a
mervecoskunn <merve_coskun2013@hotmail.com>
2024-01-19 09:36:00 +0100

    Added button test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,14 +4,20 @@
 
 const buttonClick = require("../button");
 
-beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
-    test("Expects content to change", () => {
+    test("expects p content to change", () => {
         buttonClick();
-        expect(document.getElementById("par")
-            .innerHTML).toEqual("You Clicked");
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
